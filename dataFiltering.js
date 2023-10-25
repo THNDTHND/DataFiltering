16a38a0f9446fd895b2d095fa6c1cda40b5f1d7f
Thamognya <98194153+thamognya@users.noreply.github.com>
2023-10-25 11:26:28 +0700

    Update dataFiltering.js

--- dataFiltering.js
+++ dataFiltering.js
@@ -1,47 +1,42 @@
+function main() {
+  copyDatatoAnotherSheet();
+}
+
 function copyDatatoAnotherSheet() {
-    // Main organizing sheet https://docs.google.com/spreadsheets/d/16WHoe_20PR_1AbRr-ItBdN_-R-M-RwRxKxzzRr2PmRM/edit#gid=972334631
-    var mainOrganizingSheetId = '16WHoe_20PR_1AbRr-ItBdN_-R-M-RwRxKxzzRr2PmRM';
-    var mainOrganizingSheet = SpreadsheetApp.openById(mainOrganizingSheetId).getSheetByName('KPI Response ');
-    // Folder ID, where everything is located
-    var folderID = '11mUdlP39SQBGI-W6IajEVmvkwy_xQcCo';
+  // Folder ID, where everything is located
+  var folderID = '11mUdlP39SQBGI-W6IajEVmvkwy_xQcCo';
 
-    //Get data range
-    var dataRange = mainOrganizingSheet.getDataRange();
-    var values = dataRange.getValues();
-    //Loop to find the matching name
-    for (var i = 0; i < values.length; i++) {
-      var matchName = values[i][1]; 
-      //find the matching sheet name in the folder
-      var sheetName = getSheetNameFromOrganizingSheet(mainOrganizingSheet, matchName);
-
-      if (sheetName) {
-        var exportedSheet = openSheetInFolder(folderID, matchName);
-
-        if (exportedSheet){
-          //copy entire row to the destination sheet
-          exportedSheet.appendRow(values[i]);
-        } else {
-          Logger.log('Sheet not found for' + matchName);
-        } 
-      }
-        else {
-          Logger.log('Sheet name not found for' + matchName);
-        }
-      }
-    }
-
-//Function to open sheet in another folder
-function openSheetInFolder(folderID, sheetName) {
+  // Get all files in the specified folder
   var folder = DriveApp.getFolderById(folderID);
   var files = folder.getFilesByType(MimeType.GOOGLE_SHEET);
 
-  while (files.hasNext()) {
-    var file = files.next
-    var sheet = SpreadsheetApp.openById(file.getID()).getSheetByName(sheetName);
+  // Main organizing sheet
+  var mainOrganizingSheetId = '16WHoe_20PR_1AbRr-ItBdN_-R-M-RwRxKxzzRr2PmRM';
+  var mainOrganizingSheet = SpreadsheetApp.openById(mainOrganizingSheetId).getSheetByName('KPI Response');
 
-    if (sheet) {
-      return sheet;
-    } 
+  // Get data range
+  var dataRange = mainOrganizingSheet.getDataRange();
+  var values = dataRange.getValues();
+
+  // Loop through files in the folder
+  while (files.hasNext()) {
+    var file = files.next();
+    var sheetName = file.getName().replace(/\.gsheet$/, ''); // Remove the file extension
+
+    // Loop to find the matching name
+    for (var i = 0; i < values.length; i++) {
+      var matchName = values[i][1];
+
+      if (matchName === sheetName) {
+        var exportedSheet = SpreadsheetApp.openById(file.getId()).getSheetByName(sheetName);
+
+        if (exportedSheet) {
+          // Copy entire row to the destination sheet
+          exportedSheet.appendRow(values[i]);
+        } else {
+          Logger.log('Sheet not found for ' + sheetName);
+        }
+      }
+    }
   }
-  return null
 }
